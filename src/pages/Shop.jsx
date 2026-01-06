532f439fbf34afd9233df57100d37c64e3a3b20b
naderzayan <naderahmed.na49@gmail.com>
2026-01-06 16:54:04 +0200

    ShopUpDate

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -38,6 +38,8 @@ export default function Shop() {
         <ShopCard />
         <ShopCard />
         <ShopCard />
+        <ShopCard />
+        <ShopCard />
       </div>
       <Footer />
     </main>
